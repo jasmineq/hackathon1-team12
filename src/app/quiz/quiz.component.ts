ecc9b2eeafe51f3800d39ddd598c91a6853f2b5c
Jasmine Quach <xjasminequach@gmail.com>
2017-08-20 13:55:37 -0500

    Add quiz component

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -11,17 +11,43 @@ import { IQuiz } from './../data/quiz.model';
 export class QuizComponent implements OnInit {
   quiz: IQuiz;
   questions: IQuestion[];
+  chosenAnswer: number | null;
+  numberCorrect: number;
+  currentQuestion: number;
 
   constructor(private quizService: QuizService) {}
 
   ngOnInit() {
     this.quiz = this.quizService.getQuiz(0);
     this.questions = this.quizService.getQuestions(0);
+    this.chosenAnswer = null;
+    this.numberCorrect = 0;
+    this.currentQuestion = 0;
     console.log(this.quiz);
     console.log(this.questions);
   }
 
   answer(choice) {
-    alert(choice);
+    if (this.answerIsCorrect(choice)) {
+      this.numberCorrect += 1;
+    }
+    this.chosenAnswer = choice;
+  }
+  answerIsCorrect(choice) {
+    const question = this.questions[this.currentQuestion];
+    const correctAnswer = question.choices.indexOf(question.answer);
+
+    return choice === correctAnswer;
+  }
+  nextQuestion() {
+    this.currentQuestion += 1;
+
+    if (this.currentQuestion !==              this.questions.length) {
+      this.chosenAnswer = null;
+    }
   }
 }
+
+
+
+
