2933be98a66447f0ac9083fc42ebc720907280a7
Dave <daveharmswebdev@gmail.com>
2017-08-20 15:02:12 -0500

    using firebase instead of dev data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAwEKpMkzwCeGARtUcmu7OhS-hGa5mhCHk',
+    authDomain: 'gitflash-59fdb.firebaseapp.com',
+    databaseURL: 'https://gitflash-59fdb.firebaseio.com',
+    projectId: 'gitflash-59fdb',
+    storageBucket: 'gitflash-59fdb.appspot.com',
+    messagingSenderId: '336811475006'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    apiKey: 'AIzaSyAwEKpMkzwCeGARtUcmu7OhS-hGa5mhCHk',
+    authDomain: 'gitflash-59fdb.firebaseapp.com',
+    databaseURL: 'https://gitflash-59fdb.firebaseio.com',
+    projectId: 'gitflash-59fdb',
+    storageBucket: 'gitflash-59fdb.appspot.com',
+    messagingSenderId: '336811475006'
+  },
+  production: false,
 };
