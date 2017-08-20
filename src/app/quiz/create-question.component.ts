1cdde552355d139cedda6d09f58b03b27a91ea03
Dave <daveharmswebdev@gmail.com>
2017-08-20 17:13:07 -0500

    added create quiz and create questions

--- src/app/quiz/create-question.component.ts
+++ src/app/quiz/create-question.component.ts
@@ -1,18 +1,55 @@
+import { IQuestion } from './../data/question.model';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import * as firebase from 'firebase/app';
-import { davedata, quizzes } from './../data/davedata';
+import { QuizService } from './quiz.service';
 
 @Component({
-  templateUrl: './create-question.component.html',
-  styleUrls: ['./create-question.component.css']
+  templateUrl: 'create-question.component.html',
 })
 export class CreateQuestionComponent implements OnInit {
+  quizID: number;
+  questionText: string;
+  choiceA: string;
+  choiceB: string;
+  choiceC: string;
+  choiceD: string;
+  answer: string;
+  explanation: string;
 
-  constructor(private db: AngularFireDatabase) {}
+  constructor(
+    private route: ActivatedRoute,
+    private quizService: QuizService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.quizID = +params['id'];
+      console.log(this.quizID);
+    });
+  }
 
+  createQuestion(values) {
+    const newQuestion: IQuestion = {
+      questionText: values.questionText,
+      choices: [
+        values.choiceA,
+        values.choiceB,
+        values.choiceC,
+        values.choiceD,
+      ],
+      answer: values.answer,
+      explanation: values.explanation,
+      quiz_id: this.quizID
+    };
+    console.log(newQuestion);
+    this.quizService.createQuestion(newQuestion);
+    this.questionText = '';
+    this.choiceA = '';
+    this.choiceB = '';
+    this.choiceC = '';
+    this.choiceD = '';
+    this.answer = '';
+    this.explanation = '';
   }
 
 }
