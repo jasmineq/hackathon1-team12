100779696528997862a764e43ed036cefc6e13fc
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-08-20 10:01:52 -0500

    navbar and header

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  template: `
-  <div class="navbar navbar-default">
-    <ul class="nav navbar-nav">
-      <li><a [routerLink]="['/home']">Home</a></li>
-      <li><a [routerLink]="['/quiz']">quiz</a></li>
-    </ul>
-  </div>
-  `
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'] 
 })
 export class NavbarComponent {}
