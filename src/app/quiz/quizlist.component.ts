6065efb94f9faf4885eb7c15f9240d7188e4744b
Dustin Walker <thewalkman1234@gmail.com>
2017-08-20 12:19:49 -0500

    quiz list and routes

--- src/app/quiz/quizlist.component.ts
+++ src/app/quiz/quizlist.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IQuiz } from '../data/quiz.model';
+import { quizzes } from '../data/davedata';
 
 @Component({
   templateUrl: './quizlist.component.html',
   styleUrls: ['./quizlist.component.css']
 })
-export class QuizlistComponent {
+export class QuizlistComponent implements OnInit {
+  quizzes: IQuiz[];
 
-}
+  ngOnInit() {
+    this.quizzes = quizzes;
+    console.log(this.quizzes);
+  };
+
+};
+
